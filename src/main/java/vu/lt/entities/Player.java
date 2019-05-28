4fe9a34e4a88664dba50c4c7df99b227f033be74
Tomas Brezinskas <brezinskas.tomas@gmail.com>
2019-05-28 09:40:35 +0300

    some fake backend

---
+++ src/main/java/vu/lt/entities/Player.java
@@ -0,0 +1,24 @@
+package vu.lt.entities;
+
+import java.io.Serializable;
+
+public class Player implements Serializable {
+
+    private String name;
+
+    public Player() {
+
+    }
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String setName() {
+        return this.name = name;
+    }
+}
